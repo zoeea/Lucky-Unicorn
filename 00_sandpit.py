32b124284e30b3ac37573cfe04bae1db122072b8
zoeea <99296464+zoeea@users.noreply.github.com>
2022-02-16 11:51:49 +1300

    removed sandpit number / for expt

--- 00_sandpit.py
+++ 00_sandpit.py
@@ -1,23 +1,3 @@
-print("hello world")
-get_name = input("who are you? ")
-print (get_name)
-
-# Ask users for a number 
-get_number = int(input("Choose a number? "))
-
-# multiply the number by 5 
-times_five = get_number * 5
-
-answer = "{} times five is equal to " \
-"{}".format(get_number, times_five)
-
-# Output the result 
-print(answer)
-
-# count up from one to ten
-for item in range(1, 10 + 1):
-     print(item)
- # Get name until an exit code is entered 
 Name = ""   
 while name.lower != "xxx":
     name=input( "who are you? ")
