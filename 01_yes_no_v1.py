a78a9e248e68ead3eb2c2ffea7180ad6d22e4b24
zoeea <99296464+zoeea@users.noreply.github.com>
2022-02-16 11:51:13 +1300

    Add files via upload

---
+++ 01_yes_no_v1.py
@@ -0,0 +1,14 @@
+# Ask the user if they have played before 
+show_instructions = input ("Have you played this game before? ") .lower()
+
+# If they say yes, output 'program continues'
+if show_instructions == "yes" :
+    print ("Program continues")
+
+ elif show_instructions == "no":
+     print("program continues")  
+
+# If they say no, output 'display instruction'
+else:
+    print ("please answer yes / no")
+
