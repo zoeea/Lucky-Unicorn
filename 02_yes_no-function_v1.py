f2719d116aa0dcce49c0369468186fbaefe934c0
zoeea <99296464+zoeea@users.noreply.github.com>
2022-02-18 09:57:12 +1300

    updated are you having fun etc

---
+++ 02_yes_no-function_v1.py
@@ -0,0 +1,34 @@
+
+# Functions go here...
+def yes_no(question):
+    valid = False
+    while not valid:
+        response = input (question) .lower()
+
+        if response == "yes" or response == "y" :
+            response = "yes"
+            return response
+        
+        elif response == "no" or response == "n" :
+            response = "no"
+            return response
+
+        else:
+            print ("please answer yes / no")
+            return response
+
+
+
+
+
+# Main routine goes here...
+show_instructions = yes_no ("Have you played the game before? ")    
+print ("You chose {}".format(show_instructions))
+print()
+having_fun = yes_no ("Are you having fun? ")
+print("you said {} to having fun".format(having_fun))
+
+
+
+
+
